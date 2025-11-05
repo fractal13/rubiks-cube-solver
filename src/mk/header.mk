9be6f84f8d1ed8eaf4e75e0380481286fe4e9771
Curtis Larsen <curtis.gale.larsen@gmail.com>
2025-11-05 14:49:47 -0700

    built the Makefiles myself.

---
+++ src/mk/header.mk
@@ -0,0 +1,18 @@
+CXX := g++
+AR := ar
+CXXFLAGS := -Wall -Wextra -Wpedantic -Werror -std=c++20
+ARFLAGS := Ucrus
+DEPDIR := .d
+DEPFLAGS = -MT $@ -MMD -MP -MF $(DEPDIR)/$*.d
+
+SOURCES := $(wildcard *.cpp)
+OBJECTS := $(patsubst %.cpp,%.o,$(SOURCES))
+
+AI_INCLUDE := $(PROJECT_ROOT)/src/ai-lib/include
+AI_LIB_DIR := $(PROJECT_ROOT)/src/ai-lib/lib
+AI_LIB     := $(AI_LIB_DIR)/libai.a
+CXXFLAGS += -I$(AI_INCLUDE)
+
+LDFLAGS := -L $(AI_LIB_DIR) -lai
+
+.PRECIOUS: $(DEPDIR)/%.d
---
+++ src/mk/rules.mk
@@ -0,0 +1,20 @@
+.PHONY: all clean
+
+# Make the directory
+$(DEPDIR):
+	mkdir -p $@
+
+# Cause directory to be built
+$(DEPDIR)/%.d: $(DEPDIR);
+
+# Rule to compile .cpp files into .o files and generate .d dependency files
+%.o: %.cpp
+%.o: %.cpp $(DEPDIR)/%.d
+	$(CXX) $(CXXFLAGS) $(DEPFLAGS) -c $< -o $@
+
+clean:
+	-rm -f $(OBJECTS)
+
+$(TARGET): $(OBJECTS) $(AI_LIB)
+	$(CXX) -o $@ $(OBJECTS) $(LDFLAGS)
+
