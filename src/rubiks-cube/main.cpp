7a4a52ce13dfd9b96acb9ad52145a0a8eca95fad
Curtis Larsen <curtis.gale.larsen@gmail.com>
2025-11-05 15:16:09 -0700

    removed less polished solver

--- src/rubiks-cube/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "cglCubeSolver.h"
-#include <iostream>
-#include <unistd.h>
-
-int main(int /*argc*/, char **/*argv*/) {
-  bool show_prompt = isatty(STDIN_FILENO);
-  cgl::cube::process_input_stream( std::cin, show_prompt );
-  return 0;
-}
